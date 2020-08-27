ef813b1736932c7957af582b4554617f22d1166c
mehipour <52140317+mehipour@users.noreply.github.com>
2020-08-27 12:09:28 -0400

    updated nmr object, added ppm calculation

--- utils/nmr_objects.py
+++ utils/nmr_objects.py
@@ -14,33 +14,38 @@ class NMRObject():
         self.ph1 = 0
         self.lb = 0
         self.sw = 1000
+        self.original_fid = fid
         self.fid = fid
-        self.fidlb = fid
         self.np = self.fid.shape[0]
         self.baseline = np.zeros_like(self.fid)
-        self.spectrum = fftshift(fft(self.fid))
-        self.phased_spectrum = self.spectrum
-        self.clean_spectrum = self.spectrum
+        self.ppm = np.arange(0,self.np)
+        self.get_spectrum()
 
+    def get_spectrum(self):
+        self.spectrum = fftshift(fft(self.fid))
+        
     def phase(self, ph0=0, ph1=0):
+        idx = np.linspace(-np.floor(self.np/2), np.floor(self.np/2), self.np)
+        dph0 = ph0 - self.ph0
+        dph1 = ph1 - self.ph1
+        self.spectrum = self.spectrum * np.exp(1j*(dph0+dph1*idx))
         self.ph0 = ph0
         self.ph1 = ph1
-        idx = np.linspace(-np.floor(self.np/2), np.floor(self.np/2), self.np)
-        self.phased_spectrum = self.spectrum * np.exp(1j*(self.ph0+self.ph1*idx))
         
     def line_broad(self, lb=20):
         idx = np.linspace(0, self.np, self.np)
-        self.lb = lb
-        self.fidlb = self.fid * np.exp(-self.lb*idx/self.sw)
-        self.spectrum = fftshift(fft(self.fidlb))
+        dlb = lb - self.lb
+        self.fid = self.fid * np.exp(-dlb*idx/self.sw)
+        self.get_spectrum()
         self.phase(self.ph0, self.ph1)
+        self.lb = lb
 
     def remove_baseline(self, poly_order=24):
-        x = np.real(self.phased_spectrum)
+        x = np.real(self.spectrum)
         baseline_object = BaselineRemoval(x)
         Modpoly_output = baseline_object.ModPoly(poly_order)
         Imodpoly_output = baseline_object.IModPoly(poly_order)
-        self.clean_spectrum = baseline_object.ZhangFit()
+        self.spectrum = baseline_object.ZhangFit()
 
     def show_fid(self):
         plt.plot(np.real(self.fid), label='real')
@@ -48,21 +53,25 @@ class NMRObject():
         plt.legend()
         plt.show()
     
-    def show_complex_spectrum(self):
-        plt.plot(np.real(self.phased_spectrum), label='real')
-        plt.plot(np.imag(self.phased_spectrum), label='imaginary')
+    def show_complex_spectrum(self, show_ppm=True):
+        plt.plot(self.ppm, np.real(self.spectrum), label='real')
+        plt.plot(self.ppm, np.imag(self.spectrum), label='imaginary')
         plt.legend()
         plt.show()
         
-    def show_real_spectrum(self):
-        plt.plot(np.real(self.phased_spectrum), label='real')
+    def show_real_spectrum(self, show_ppm=True):
+        plt.plot(self.ppm, np.real(self.spectrum), label='real')
         plt.legend()
         plt.show()
         
-    def show_abs_spectrum(self):
-        plt.plot(np.absolute(self.phased_spectrum), label='magnitude')
+    def show_abs_spectrum(self, show_ppm=True):
+        plt.plot(self.ppm, np.absolute(self.spectrum), label='magnitude')
         plt.legend()
         plt.show()
+
+    def find_ppm(self, center_freq_mhz, center_ppm):
+        bw = np.linspace(-self.sw/2, self.sw/2, self.np)
+        self.ppm = bw / center_freq_mhz + center_ppm
 
     def __getattr__(self, name):
         if name == 'ph':
@@ -82,10 +91,10 @@ class NMRObject():
                 "fid", "fidlb", "spectrum", "phased_spectrum", "baseline")
 
 
-# define Experiment object
-class EXPbject():
-    ''' Object to define experiment
-    '''
-    def __init__(self, fid):
-        pass
+# # define Experiment object
+# class EXPbject():
+#     ''' Object to define experiment
+#     '''
+#     def __init__(self, fid):
+#         pass
         
